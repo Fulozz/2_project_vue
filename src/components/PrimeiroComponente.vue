99ae1be0517deafb510522172f8bbd0cbc8be619
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-06-29 16:24:28 -0300

    confirmar

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Olá vue</h1>
+</template>
+
+<script>
+export default {
+    name: 'PrimeiroComponente'
+}
+</script>
+
+<style scoped>
+    h1{
+        text-align: center;
+    }
+</style>
