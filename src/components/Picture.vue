d5d2700b169571c54ff541da6b9fea648c1b361c
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-07-03 18:08:58 -0300

    Reutilization test

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,9 +1,15 @@
 <template>
+    <picture>
     <img :src="avatar" :alt="descricao">
+    <Reutilizacao />
+    </picture>
 </template>
 
 <script>
+import Reutilizacao from './Reutilizacao.vue'
+
 export default {
+  components: { Reutilizacao },
     nome: 'Picture',
     data(){
         return{
