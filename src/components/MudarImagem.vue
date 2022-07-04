78b8ce415daa19c097d1244f864c1be623f8fa58
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-07-04 19:14:14 -0300

    Final da Parte Teorica

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImagem')">Trocar imagem de perfl</button>
+    </div>    
+</template>
+<script>
+export default {
+    name: 'MudarImagem',
+
+    emits: ['mudarImagem']
+}
+</script>
+<style scoped>
+
+</style>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,19 +1,27 @@
 <template>
     <picture>
     <img :src="avatar" :alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem" />
     </picture>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue'
 import Reutilizacao from './Reutilizacao.vue'
 
 export default {
-  components: { Reutilizacao },
+  components: { Reutilizacao, MudarImagem },
     nome: 'Picture',
     data(){
         return{
             avatar: "/img/avatar.png",
             descricao: "Thiago Silva Andrade"
+        }
+    },
+    methods:{
+        trocarImagem(){
+            this.avatar = "/img/avatar2.png"
+ 
         }
     }
 }
